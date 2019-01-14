481ddd24e668eb3b0efabb78cceff364f5696600
sarthakgupta898 <45997129+sarthakgupta898@users.noreply.github.com>
2019-01-14 22:59:29 +0530

    Add files via upload

---
+++ 3.pointers and functions/pass by reference.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Pass by Reference using Pointers
+void increment(int *aptr){
+    *aptr = *aptr + 1;
+    cout<<"Inside Function "<<*aptr<<endl;
+}
+
+int main(){
+
+    int a=10;
+    increment(&a);
+    cout<<"Inside Main : " <<a;
+    return 0;
+}
